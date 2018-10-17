f8c566172efaf08301edfc44615a3e480a7d0d32
Izik Orgad <izik.orgad@bitmain.com>
2018-10-17 18:01:36 +0300

    cont

--- src/configManager.js
+++ src/configManager.js
@@ -26,7 +26,7 @@ const configFileExists = (configFile = configFileName) => {
     if (e.code == 'ENOENT') {
       console.log(`Config file not found at ${configFile}.`);
     } else {
-      console.log(`Config file check error at ${configFile}: ${e}` );
+      console.log(`Config file check error at ${configFile}: ${e}`);
     }
     return false;
   }
@@ -36,7 +36,7 @@ export const init = (defaultConfig, newConfigFileName, cb) => {
   try {
     console.log('Config initializing...');
 
-    if(newConfigFileName != null) {
+    if (newConfigFileName != null) {
       configFileName = newConfigFileName;
     }
 
@@ -59,6 +59,9 @@ export const init = (defaultConfig, newConfigFileName, cb) => {
 };
 
 export const getConfig = () => {
+  if (!config) {
+    readConfig();
+  }
   return config;
 };
 
@@ -73,8 +76,6 @@ const readConfig = (configFile = configFileName) => {
 
   config = parseConfigContent(content);
   console.log(`config read: ${JSON.stringify(config)}, file: ${resolvedPath}.`);
-
-  return config;
 };
 
 const parseConfigContent = (content) => {
